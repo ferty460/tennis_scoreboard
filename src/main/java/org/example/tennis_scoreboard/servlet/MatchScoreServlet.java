dec5778c4391704dfd4b0c5c95a2d9b91ce8a807
ferty460 <rostovcevvladimir460@gmail.com>
2025-08-18 19:25:34 +0300

    feat(router): routing to the match score page is set up

---
+++ src/main/java/org/example/tennis_scoreboard/servlet/MatchScoreServlet.java
@@ -0,0 +1,19 @@
+package org.example.tennis_scoreboard.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/match-score")
+public class MatchScoreServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("match_score.jsp").forward(req, resp);
+    }
+
+}
